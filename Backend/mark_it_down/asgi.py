b02ac7e27fe42286847f118c9436a45d256cd3d8
selmanfariz18 <selmanulfarizyka@gmail.com>
2024-11-12 19:35:32 +0530

    project initialized

---
+++ Backend/mark_it_down/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mark_it_down project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mark_it_down.settings')
+
+application = get_asgi_application()
