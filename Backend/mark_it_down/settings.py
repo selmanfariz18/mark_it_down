bb1202deea29d0f9ae676f0d5968ee0e3b8c3228
selmanfariz18 <selmanulfarizyka@gmail.com>
2024-11-17 20:15:45 +0530

    for production

--- Backend/mark_it_down/settings.py
+++ Backend/mark_it_down/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(^eq@mczuq^b)eh*_j+ycc-tg!h62%!#uo-1mm)3xl*)i1byuu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
